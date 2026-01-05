9ac866b05617531896a389c66723c471c8529da6
Jatin023 <94826565+Jatin023@users.noreply.github.com>
2026-01-05 15:02:43 +0530

    add bubble sort

---
+++ Sorting/bubble.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class bubble {
+     public static void main(String[] args)
+     {
+        int a[]={4,1,5,2,3};
+        int n=a.length;
+
+        for(int i=0;i<n-1;i++)
+        {
+            for(int j=0;j<n-i-1;j++)
+            {
+                if(a[j]>a[j+1])
+                {
+                    int temp=a[j];
+                    a[j]=a[j+1];
+                    a[j+1]=temp;
+
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(a[i]);
+        }
+     }
+}
