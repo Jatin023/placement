ea1a624e0bdc5a16202ef59c7ecdce42fda25885
Jatin023 <94826565+Jatin023@users.noreply.github.com>
2026-01-08 01:25:30 +0530

    change in fibonacci println to print

--- Code/fibonacci.java
+++ Code/fibonacci.java
@@ -5,11 +5,11 @@ public class fibonacci {
         int n=10;
         int a=0,b=1;
 
-        System.out.println(a +" " +b+" ");
+        System.out.print(a +" " +b+" ");
         for(int i=2;i<n;i++)
         {
             int c=a+b;
-            System.out.println(c+" ");
+            System.out.print(c+" ");
             a=b;
             b=c;
         }
